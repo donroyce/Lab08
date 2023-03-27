1ae4ccc34233bcc6d26c713d9841770b33bb5e9b
donroyce <129000759+donroyce@users.noreply.github.com>
2023-03-27 00:05:30 -0400

    Delete crypto.py

--- crypto.py
+++
@@ -1,9 +0,0 @@
-Python 3.10.10 (v3.10.10:aad5f6a891, Feb  7 2023, 08:47:40) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-============= RESTART: /Users/macos/Documents/PythonFiles/crypto.py ============
->>> import hashlib
->>> import hashlib
->>> 
->>> print (hashlib.md5(b"hello").hexdigest())
-5d41402abc4b2a76b9719d911017c592
